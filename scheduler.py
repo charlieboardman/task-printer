613e7b523cecea373cb40c8038ebfb6283145710
Charlie Boardman <cboardman2@gmail.com>
2023-10-25 19:36:55 -0700

    Changed tracker.csv to put all jobs into one column, and put an interval column in. The tracker is now more easily accessible with dot notation using pandas, i.e. tracker.job[3] returns the name of the job at row index 3. The rows now line up with job id as well. This should make scheduler.py more intuitive and easier to read/write.

--- scheduler.py
+++ scheduler.py
@@ -19,11 +19,15 @@ _4mo_ID_col = 4
 _12mo_ID_col = 7
 _4mo_shop_ID_col = 10
 
+_2mo_job_col = 0
+_4mo_job_col = 3
+_12mo_job_col = 6
+_4mo_shop_job_col = 9
 
 #Establish today
 today = date.today()
 
-#Create a function to calculate the weeks since a job was performed
+#Calculate the weeks since a job was performed
 def weekssince(wk,yr,today):
     if yr == today.year:
         return today.isocalendar().week - wk
@@ -31,6 +35,26 @@ def weekssince(wk,yr,today):
         return (52-wk) + today.isocalendar().week
     else:
         return (52-wk) + 52*(today.year - yr)
+
+
+#Check if a task already exists in todo.txt for a specific vehicle
+def task_exists(ID,vehicle):
+    with open('todo.txt','r') as todo:
+        for row in todo:
+            if f'@{vehicle}' and f'#{ID}' in row.split():
+                return True
+        return False
+    
+
+#Add a task to todo.txt
+#Need to complete this function. tracker.csv change lets me use dot notation
+def add_task(ID,vehicle,tracker_df):
+    
+    for 
+    
+    with open('todo.txt','a') as todo:
+        todo.write(f"{job} @black4runner due:2016-07-30 #3")       
+    
 
 vehicles_tracking = os.path.join(os.getcwd(),'vehicles_tracking')
 
@@ -44,5 +68,5 @@ for d in os.listdir(vehicles_tracking):
         
         #check 2month
         wk,yr = tracker_df.iloc[row,_2mo_lastwk_col].split('-')
-        if weekssince(int(wk),int(yr),today) >= 9:
+        if weekssince(int(wk),int(yr),today) >= 9 and not task_exists(ID):
             add_task(ID) #Need to create this function
