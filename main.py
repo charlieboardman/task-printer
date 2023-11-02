87f8d546172d2578f52b11e0422ba4a444a2861a
charlieboardman <cboardman2@gmail.com>
2023-11-01 22:05:50 -0700

    Rewrote everything to operate as functions. It's easier to read and follow now. Also added main.py to call these functions in sequence.

---
+++ main.py
@@ -0,0 +1,25 @@
+import scheduler
+import renderer
+import cups
+from datetime import date, timedelta
+
+today = date.today()
+today_str = today.strftime('%d-%b-%Y')
+
+#Read todo.txt to list active bundles
+active_bundles = scheduler.find_active_bundles()
+
+#Update the todo.txt list with jobs that are due
+scheduler.schedule_tasks(active_bundles)
+
+#Write active bundles to active.txt
+scheduler.write_active_bundles(active_bundles)
+
+#Render the list
+renderer.render_list()
+
+#Print the rendered list
+#conn = cups.Connection()
+#printers = conn.getPrinters()
+#printer_name = list(printers.keys())[0]
+#conn.printFile(printer_name, 'rendered.pdf', f'Tasks list: {today_str}', {})
--- renderer.py
+++ renderer.py
@@ -10,96 +10,101 @@ def isdate(word):
         if not (char.isnumeric() or char == '-' or char == '/'):
             return False
         return True
-    
-#We need a task object with all the todo.txt information
-class task:
-    def __init__(self,vehicle,desc,id_,priority=None,due=None):
-        self.vehicle = vehicle
-        self.desc = desc
-        self.id_ = id_
-        self.priority = priority
-        self.due = due
+
+
+def render_list():
+
+    #We need a task object with all the todo.txt information
+    class task:
+        def __init__(self,vehicle,desc,id_,priority=None,due=None):
+            self.vehicle = vehicle
+            self.desc = desc
+            self.id_ = id_
+            self.priority = priority
+            self.due = due
+
 
 #Read the todo.txt file into a structure that we can put down into html
-with open('todo.txt','r') as todo:
-    
-    #This dictionary will hold all the tasks and group them by vehicle.
-    #The vehicle name is the key, and a list of the task objects for that vehile is the value
-    tasks_v = {}
-    
-    #We are going to go over every line of the todo file. In each line, we identify which vehicle the task pertains to and the details of the tasks
-    for line in todo:
+    with open('todo.txt','r') as todo:
         
-        #Pass over empty lines
-        if line == '' or line == '\n':
-            continue
+        #This dictionary will hold all the tasks and group them by vehicle.
+        #The vehicle name is the key, and a list of the task objects for that vehile is the value
+        tasks_v = {}
         
-        #Identify the vehicle
-        vehicle = next((word for word in line.split() if word.startswith('@')),None)
-        
-        #Initialize the list where the tasks for that vehicle will go, if needed
-        if vehicle not in tasks_v.keys():
-            tasks_v[vehicle] = []
+        #We are going to go over every line of the todo file. In each line, we identify which vehicle the task pertains to and the details of the tasks
+        for line in todo:
             
-        #Identify the task description from the normal words in the line
-        desclist = [word for word in line.split() if not any([isdate(word),word.startswith(( '@', 'due:', '(', '#', '%'))])]
-        desc = ' '.join(desclist)
+            #Pass over empty lines
+            if line == '' or line == '\n':
+                continue
+            
+            #Identify the vehicle
+            vehicle = next((word for word in line.split() if word.startswith('@')),None)
+            
+            #Initialize the list where the tasks for that vehicle will go, if needed
+            if vehicle not in tasks_v.keys():
+                tasks_v[vehicle] = []
+                
+            #Identify the task description from the normal words in the line
+            desclist = [word for word in line.split() if not any([isdate(word),word.startswith(( '@', 'due:', '(', '#', '%'))])]
+            desc = ' '.join(desclist)
+            
+            #Identify priority because it starts with (
+            #priority = next((word for word in line.split() if word.startswith('(')))
+            
+            #Identify the due date
+            due_str = next((word for word in line.split() if word.startswith('due:')))[4::]
+            year,month,day = [int(x) for x in due_str.split('-')]
+            due = date(year,month,day)
+            due_fmt = due.strftime('%d-%b-%Y')
+            
+            #Identify the task ID
+            id_ = next((word for word in line.split() if word.startswith('#')))
+            
+            #Put all that into the list of tasks for each vehicle
+            tasks_v[vehicle].append(task(vehicle,desc,id_,due_fmt))
+            
+    #Now actually create the html
+
+    header = f"Tasks for week of {date.today().strftime('%B')} {date.today().day}"
+    html = f"<html><h1>{header}</h1>"
+
+    head = """<head>
+      <style>
+        [data-prefix^='h'] {
+          margin-top: 10;
+          margin-bottom: 10;
+        }
         
-        #Identify priority because it starts with (
-        #priority = next((word for word in line.split() if word.startswith('(')))
+        p {
+          margin-top: 0;
+          margin-bottom: 0;
+          font-size: 24;
+        }
         
-        #Identify the due date
-        due_str = next((word for word in line.split() if word.startswith('due:')))[4::]
-        year,month,day = [int(x) for x in due_str.split('-')]
-        due = date(year,month,day)
-        due_fmt = due.strftime('%d-%b-%Y')
+        input[type='checkbox'] {
+          margin-right: 10px;
+        }
         
-        #Identify the task ID
-        id_ = next((word for word in line.split() if word.startswith('#')))
-        
-        #Put all that into the list of tasks for each vehicle
-        tasks_v[vehicle].append(task(vehicle,desc,id_,due_fmt))
-        
-#Now actually create the html
+      </style>
+    </head>"""
 
-header = f"Tasks for week of {date.today().strftime('%B')} {date.today().day}"
-html = f"<html><h1>{header}</h1>"
+    html += head
+    html += "<body>"
 
-head = """<head>
-  <style>
-    [data-prefix^='h'] {
-      margin-top: 10;
-      margin-bottom: 10;
-    }
-    
-    p {
-      margin-top: 0;
-      margin-bottom: 0;
-      font-size: 24;
-    }
-    
-    input[type='checkbox'] {
-      margin-right: 10px;
-    }
-    
-  </style>
-</head>"""
+    #Task sections generator
+    h_num = 2 #Start with <h2> since we use <h1> for the main title header
+    for vehicle in tasks_v.keys():
+        html += f"<p><h{h_num} data-prefix='h'>{vehicle}</h{h_num}>"
+        tnum = 1
+        for task in tasks_v[vehicle]:
+            html += f"<p><input type='checkbox' id='{vehicle}{tnum}'><label for='{vehicle}{tnum}'>{task.desc} ({task.id_})</label></p>"
+            tnum += 1
 
-html += head
-html += "<body>"
+    html_closer = "</body></html>"
+    html += html_closer
 
-#Task sections generator
-h_num = 2 #Start with <h2> since we use <h1> for the main title header
-for vehicle in tasks_v.keys():
-    html += f"<p><h{h_num} data-prefix='h'>{vehicle}</h{h_num}>"
-    tnum = 1
-    for task in tasks_v[vehicle]:
-        html += f"<p><input type='checkbox' id='{vehicle}{tnum}'><label for='{vehicle}{tnum}'>{task.desc} ({task.id_})</label></p>"
-        tnum += 1
-
-html_closer = "</body></html>"
-html += html_closer
-
-pdfkit.from_string(html,'rendered.pdf')
+    pdfkit.from_string(html,'rendered.pdf')
+    return(html)
 
 #print(html)
